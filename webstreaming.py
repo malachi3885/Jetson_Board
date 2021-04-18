66e9b3f2eeb21817e17994175af3e0318032dcc4
malachi3885 <malachi3885@gmail.com>
2021-04-18 16:21:06 +0700

    no image, no css

--- webstreaming.py
+++ webstreaming.py
@@ -15,6 +15,7 @@ from faceDetectorAndAlignment import faceDetectorAndAlignment
 from faceEmbeddingExtractor import faceEmbeddingExtractor
 import requests
 import json
+import os
 
 outputFrame = None
 lock = threading.Lock()
@@ -28,7 +29,7 @@ inputStream = cv2.VideoCapture(0)
 app.config['SECRET_KEY'] = 'tempKey'
 
 #TODO
-#cron = CronTab(user='root')
+# cron = CronTab(user='root')
 
 detector = faceDetectorAndAlignment('models/faceDetector.onnx', processScale=0.5)
 embeddingExtractor = faceEmbeddingExtractor('models/r100-fast-dynamic.onnx')
@@ -115,21 +116,11 @@ def writetime(hour, minute, endhour, endminute, day, month, n):
 	job.month.on(month)
 	cron.write()
 """
-#TODO ลบหน้าแรกไปไว้ในคอม
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-	if request.method == 'POST':
-		room = request.form['room']
-		#TODO redirects to ห้อง 
-		# return redirect('http://www.google.com')
-		return redirect(url_for('choose_action', room=room))
-	else:
-		return render_template('main.html')
 
 
 @app.route('/choose_action_<room>')
-def choose_action(room):
+def index(room):
 	return  render_template('choose_action.html', room=room)
 
 @app.route('/clock', methods=['GET','POST'])
@@ -174,60 +165,107 @@ def gen():
         (flag, encoedImage) = cv2.imencode(".jpg",frame)
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encoedImage) + b'\r\n')
 
+students = list()
+student = dict({'id': '6030631621', 'name': 'Itsara'})
+students.append(student)
+
 @app.route('/room<room>/course<course>/live', methods=['POST', 'GET'])
 def live(room, course):
-	students = list()
-	if request.method == 'POST':
-		if request.form['button'] == 'check':
-			faces = np.load('./storeEmbedding/embedding.npy', allow_pickle=True)
-			if (faces.size!=0):
-				name = np.load('./storeEmbedding/name.npy', allow_pickle=True)
-				ids = np.load('./storeEmbedding/id.npy', allow_pickle=True)
+    img = cv2.imread('./img.png')
 
-				
-				faces = faces.reshape((name.shape[0],512))
-				isFrameOK, inputFrame = inputStream.read()
-				### save name owner
-				if isFrameOK:
-					
-					faceBoxes, faceLandmarks, alignedFaces = detector.detect(inputFrame)
-					if len(faceBoxes) > 0:
-						student = dict()
-						### Extract embedding ###
-						extractEmbedding = embeddingExtractor.extract(alignedFaces)
-						# Compare embedding
-						distance = cdist(faces, extractEmbedding)
-						a = []
-						if len(distance):
-							for i in range(len(distance[0])):
-								b = []
-								for j in range(len(distance)):
-									b.append(distance[j][i])
-								a.append(b)
-						distance = a
+    if request.method == 'POST':
+        if request.form['button'] == 'check':
+            faces = np.load('./storeEmbedding/embedding.npy', allow_pickle=True)
+            if (faces.size!=0):
+                name = np.load('./storeEmbedding/name.npy', allow_pickle=True)
+                ids = np.load('./storeEmbedding/id.npy', allow_pickle=True)
+                gpax = np.load('./storeEmbedding/gpax.npy', allow_pickle=True)
 
-						### Draw face ##
-						for faceIdx, faceBox in enumerate(faceBoxes):
-							### find min distance
-							dis_face = distance[faceIdx]
-							# print(dis_face)
-							if np.min(dis_face) < 0.9:
-								student['id'] = ids[np.where(dis_face == np.min(dis_face))[0]][0]
-								student['name'] = name[np.where(dis_face == np.min(dis_face))[0]][0]
-								students.append(student)
+                
+                faces = faces.reshape((name.shape[0],512))
+                isFrameOK, inputFrame = inputStream.read()
+                ### save name owner
+                if isFrameOK:
+                    faceBoxes, faceLandmarks, alignedFaces = detector.detect(inputFrame)
+                    if len(faceBoxes) > 0:
+                        student = dict()
+                        ### Extract embedding ###
+                        extractEmbedding = embeddingExtractor.extract(alignedFaces)
+                        # Compare embedding
+                        distance = cdist(faces, extractEmbedding)
+                        a = []
+                        if len(distance):
+                            for i in range(len(distance[0])):
+                                b = []
+                                for j in range(len(distance)):
+                                    b.append(distance[j][i])
+                                a.append(b)
+                        distance = a
+
+                        for faceIdx, faceBox in enumerate(faceBoxes):
+                            x1, y1, x2, y2 = faceBox[0:4].astype(np.int)
+                            
+                            ### find min distance
+                            dis_face = distance[faceIdx]
+                            # print(dis_face)
+                            if np.min(dis_face) < 0.9:
+                                cv2.rectangle(inputFrame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                                student = dict()
+                                student['id'] = ids[np.where(dis_face == np.min(dis_face))[0]][0]
+                                student['name'] = name[np.where(dis_face == np.min(dis_face))[0]][0]
+                                gpa = gpax[np.where(dis_face == np.min(dis_face))[0]][0]
+                                print('student')
+                                print(student)
+                                students.append(student)
+                                cv2.putText(inputFrame, student['name']+ ' GPAX: ' + str(gpa), (x1,y1-5), cv2.FONT_HERSHEY_COMPLEX, 0.7,(255,255,255),2)   
+                            else:
+                                cv2.rectangle(inputFrame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                                cv2.putText(inputFrame, 'Unknowed', (x1,y1-5), cv2.FONT_HERSHEY_COMPLEX, 0.7,(0,0,255),2) 
+                            status = cv2.imwrite('./img.png',inputFrame)
+                            print(status)	
+                        
+                            
+                            
 
 
-		elif request.form['button'] == 'clear':
-			faces = np.load('./storeEmbedding/embedding.npy', allow_pickle=True)
+        elif request.form['button'] == 'clear':
+            
+            faces = np.load('./storeEmbedding/embedding.npy', allow_pickle=True)
 
-			print('mickey')
-			print(faces.size)
-			
-			students.clear()
-		elif request.form['butotn'] == 'back' :
-			redirect(url_for('course', room=room))
+            print('mickey2')
+            print(students)
+            
+            students.clear()
+        elif request.form['button'] == 'add' :
+            student = dict()
+            
+            add_id = request.form['add_id']
+            add_name = request.form['add_name']
+            if (len(add_id) == 10 and len(add_name) > 0):
+                student['id'] = add_id
+                student['name'] = add_name
+                students.append(student)
+        elif request.form['button'] == 'confirm check' :
+            url = 'https://face-senior.herokuapp.com/addAttendants'
+            #TODO แก้backend
+            date = datetime.date.today().strftime("%d/%m/%Y")
 
-	return render_template('live_stream.html', room=room, course=course, students=students)
+            add_students = list()
+            for student in students:
+                add_student = dict()
+                add_student['student_id'] = student['id']
+                add_students.append(add_student)
+            payload = {"course_id" : course, "semester": 2, "date": date, "academic_year": 2564,  "students": add_students}
+            
+            
+            print(payload)
+
+            res = requests.post(url, json=payload, allow_redirects=True)
+            result = json.loads(res.content)
+            print('res')
+            print(result)
+
+    return render_template('live_stream.html', room=room, course=course, students=students, img=img)
 
 
 @app.route('/video_feed')
@@ -239,10 +277,9 @@ def course(room):
     if request.method == 'POST':
         count = True
         course = request.form['course']
-        #TODO ใส่courseให้ถูก
         url = 'https://face-senior.herokuapp.com/fetchStudent'
 
-        payload = {'course_id' : course, 'semester': 2, 'acedamic_year': 2564}
+        payload = {'course_id' : course, 'semester': 2, 'academic_year': 2564}
 
         res = requests.post(url, json=payload, allow_redirects=True)
         result = json.loads(res.content)
@@ -251,10 +288,14 @@ def course(room):
             embed = []
             names = []
             ids = []
+            gpaxs = []
+
 
             np.save('./storeEmbedding/embedding.npy', np.array([]))
             np.save('./storeEmbedding/name.npy', np.array([]))
             np.save('./storeEmbedding/id.npy', np.array([]))
+            np.save('./storeEmbedding/gpax.npy', np.array([]))
+
 
         else :
             print('def')
@@ -264,14 +305,18 @@ def course(room):
                     embed = []
                     names = []
                     ids = []
+                    gpaxs = []
                     np.save('./storeEmbedding/embedding.npy', np.array([]))
                     np.save('./storeEmbedding/name.npy', np.array([]))
                     np.save('./storeEmbedding/id.npy', np.array([]))
+                    np.save('./storeEmbedding/gpax.npy', np.array([]))
 
                 else:
                     embed = np.load('./storeEmbedding/embedding.npy', allow_pickle=True)         
                     names = np.load('./storeEmbedding/name.npy', allow_pickle=True)
                     ids = np.load('./storeEmbedding/id.npy', allow_pickle=True)
+                    gpaxs = np.load('./storeEmbedding/gpax.npy', allow_pickle=True)
+
 
                 embed = np.append(embed, [student['embedded_face']])
                 np.save('./storeEmbedding/embedding.npy', embed)
@@ -282,11 +327,14 @@ def course(room):
                 ids = np.append(ids, [student['student_id']])
                 np.save('./storeEmbedding/id.npy', ids) 
 
+                gpaxs = np.append(gpaxs, [student['gpax']])
+                np.save('./storeEmbedding/gpax.npy', gpaxs) 
+
                 count = False
         return redirect(url_for('live', room=room, course=course))
     else:
         url = 'https://face-senior.herokuapp.com/fetchCourse'
-        payload = {'semester': 2, "acedamic_year": 2564 }
+        payload = {'semester': 2, "academic_year": 2564 }
 
         res = requests.post(url, json=payload, allow_redirects=True)
 
